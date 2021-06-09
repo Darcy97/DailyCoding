3e863adc7580def43878fbf04ce32f04847e7d72
Darcy <Darcy97zh@gmail.com>
2021-06-09 17:33:32 +0800

    [code] C-Sharp in depth 分解

---
+++ Assets/Scripts/DarcyStudio/CSharpInDepth/Tuple/SuperValueAssign.cs
@@ -0,0 +1,68 @@
+/***
+ * Created by Darcy
+ * Date: Wednesday, 09 June 2021
+ * Time: 16:49:04
+ * Description: ValueAssign
+ ***/
+
+namespace DarcyStudio.CSharpInDepth.Tuple
+{
+    public class SuperValueAssign
+    {
+
+    }
+
+    public class Point
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public Point (double x, double y)
+        {
+            (X, Y) = (x, y);
+        }
+
+        /*
+        /// <summary>
+        /// Spit
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        internal void Deconstruct (out double x, out double y)
+        {
+            (x, y) = (X, Y);
+        }
+        */
+
+    }
+
+    public static class PointExtension
+    {
+
+        /// <summary>
+        /// 若目标类型无法添加 Deconstruct 
+        /// 也可以通过拓展实现
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public static void Deconstruct (this Point point, out double x, out double y)
+        {
+            (x, y) = (point.X, point.Y);
+        }
+
+        //  这里有个有意思的事情
+        //  Rider 2020.3 会推荐我写成下面这样
+        //  通过隐式调用分解方法获取 x，y
+        //  初一看没问题，但是仔细一看 😧
+        //  该方法本身就是分解方法，所以就会自己调用自己，造成死循环 😨
+        //  我还不死心的测试了一下，确实会死循环，断着点敲下了这部分代码，看来不能无脑信奉 Rider 😬（
+        //  Rider 的推荐也不是万能的哈哈哈 🤡🤡
+        // public static void Deconstruct (this Point point, out double x, out double y)
+        // {
+        //     var (d, d1) = point; //The requested operation caused a stack overflow.
+        //     (x, y)      = (d, d1);
+        // }
+
+    }
+}
---
+++ Assets/Scripts/DarcyStudio/CSharpInDepth/Tuple/VariableSplit.cs
@@ -0,0 +1,40 @@
+/***
+ * Created by Darcy
+ * Date: Wednesday, 09 June 2021
+ * Time: 16:06:37
+ * Description: Tuple Split
+ ***/
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DarcyStudio.CSharpInDepth.Tuple
+{
+    public class VariableSplit:MonoBehaviour
+    {
+        private void Test ()
+        {
+            var tuple = (10, "text");
+            var (a, b)     = tuple; //隐式分解
+            (var c, var d) = tuple; //显示分解
+
+            int    e;
+            string f;
+            (e, f) = tuple;
+
+
+            //非元组的分解
+            var p = new Point (2, 4);
+            var (x, y) = p;
+
+            // p.Deconstruct (out var x1, out var x2);
+        }
+
+        private void Start ()
+        {
+            Test ();
+        }
+    }
+
+}
