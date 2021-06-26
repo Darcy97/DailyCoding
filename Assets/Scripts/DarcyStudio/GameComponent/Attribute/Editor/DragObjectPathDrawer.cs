2a0111c9dece79fa4ef25cd6a6d7850f3e38abdc
Darcy <Darcy97zh@gmail.com>
2021-06-26 17:50:22 +0800

    [code] 支持拖拽赋值路径

---
+++ Assets/Scripts/DarcyStudio/GameComponent/Attribute/Editor/DragObjectPathDrawer.cs
@@ -0,0 +1,110 @@
+/***
+ * Created by Darcy
+ * Date: Saturday, 26 June 2021
+ * Time: 16:00:52
+ * Description: Description
+ ***/
+
+using DarcyStudio.GameComponent.Attribute.SelectObjectAttribute;
+using UnityEditor;
+using UnityEngine;
+
+namespace DarcyStudio.GameComponent.Attribute.Editor
+{
+    [CustomPropertyDrawer (typeof (DragObjectPath))]
+    public class DragObjectPathDrawer : PropertyDrawer
+    {
+
+        private const string ObjectSelectorUpdated = "ObjectSelectorUpdated";
+
+        private string _prefabName;
+
+        public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!(attribute is DragObjectPath drag))
+                return;
+
+            var btnRect = new Rect (position);
+            position.width -= 60;
+            btnRect.x      += btnRect.width - 60;
+            btnRect.width  =  60;
+            EditorGUI.BeginProperty (position, label, property);
+            EditorGUI.PropertyField (position, property, true);
+            if (GUI.Button (btnRect, "select"))
+            {
+                if (drag.SearchType == SearchType.Prefab)
+                {
+                    EditorGUIUtility.ShowObjectPicker<GameObject> (null, false, drag.SearchFilter, 100);
+                }
+                else
+                    EditorGUIUtility.ShowObjectPicker<Object> (null, false, drag.SearchFilter, 100);
+            }
+
+            var commandName = Event.current.commandName;
+            if (commandName == ObjectSelectorUpdated && EditorGUIUtility.GetObjectPickerControlID () == 100)
+            {
+                var picker = EditorGUIUtility.GetObjectPickerObject ();
+                var tPath  = AssetDatabase.GetAssetPath (picker);
+                var result = tPath.Substring (0, tPath.IndexOf ('.'));
+                property.stringValue = result;
+                _prefabName           = result;
+            }
+
+            //拖拽先写一个只支持 GameObject 的
+            if (drag.SearchType == SearchType.Prefab)
+            {
+                var prefab = GetDragObject<GameObject> (position, Event.current);
+                if (prefab)
+                {
+                    //在这里给他赋值就是不行 必须在外面赋值 可能是由于 unity 的时序逻辑导致的
+                    // property.stringValue = prefab.name;
+                    var tPath  = AssetDatabase.GetAssetPath (prefab);
+                    var result = tPath.Substring (0, tPath.IndexOf ('.'));
+                    _prefabName = result;
+                }
+
+                if (!string.IsNullOrEmpty (_prefabName) && property.stringValue != _prefabName)
+                {
+                    property.stringValue = _prefabName;
+                    EditorUtility.SetDirty (property.serializedObject.targetObject);
+                }
+            }
+
+            EditorGUI.EndProperty ();
+        }
+
+        /// <summary>
+        /// 检查是否拖拽到指定区域
+        /// </summary>
+        /// <typeparam name="T">指定类型资源</typeparam>
+        /// <param name="rect">监测区域</param>
+        /// <param name="event">事件</param>
+        /// <returns></returns>
+        private static T GetDragObject<T> (Rect rect, Event @event) where T : UnityEngine.Object
+        {
+            T @object = default;
+            if (!rect.Contains (@event.mousePosition))
+                return @object;
+            if (DragAndDrop.objectReferences.Length <= 0)
+                return @object;
+            if (DragAndDrop.objectReferences[0].GetType () == typeof (T))
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Link;
+                
+                if (@event.type != EventType.DragExited && @event.type != EventType.DragUpdated)
+                    return @object;
+                
+                DragAndDrop.AcceptDrag ();
+                GUI.changed = true;
+                @object     = (T) DragAndDrop.objectReferences[0];
+                Event.current.Use ();
+            }
+            else
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.None;
+            }
+
+            return @object;
+        }
+    }
+}
--- Assets/Scripts/DarcyStudio/GameComponent/Attribute/SelectObjectAttribute/DragObjectPath.cs
+++ Assets/Scripts/DarcyStudio/GameComponent/Attribute/SelectObjectAttribute/DragObjectPath.cs
@@ -12,6 +12,8 @@
  * 每次要手动 copy 真的烦，本想通过拖动实现，无奈重写 Inspector 修改 Mono 内的属性 通过拖拽文件可以搞定，但是二级属性就搞不定
  * 所以退而求其次 通过 PropertyDrawer 同时使用选 Object 窗口实现自动填充路径
  * 优秀的一点是这样做通用性提高了，任何字符串属性都可以使用该 Attribute, 且可以支持任何 Object 类型，有其他类型需要增加枚举就好了
+ * 更新！！！！
+ * 拖拽也支持了 哈哈哈哈哈
  ***/
 
 using UnityEngine;
@@ -19,12 +21,12 @@ using UnityEngine;
 namespace DarcyStudio.GameComponent.Attribute.SelectObjectAttribute
 {
     
-    public class SelectObjectPath : PropertyAttribute
+    public class DragObjectPath : PropertyAttribute
     {
         public readonly SearchType SearchType;
         public readonly string     SearchFilter;
 
-        public SelectObjectPath (SearchType a, string label = "")
+        public DragObjectPath (SearchType a, string label = "")
         {
             SearchType = a;
             var searchLabel = string.IsNullOrEmpty (label) ? string.Empty : $"l:{label}";
