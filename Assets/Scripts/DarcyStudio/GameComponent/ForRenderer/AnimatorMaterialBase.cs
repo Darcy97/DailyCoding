7030f9b94337943135ba316f09505e5ae827b9a9
Darcy <Darcy97zh@gmail.com>
2021-06-29 19:19:07 +0800

    [优化] 优化一下这个 AnimatorMaterial

--- Assets/Scripts/DarcyStudio/GameComponent/ForRenderer/AnimatorMaterialBase.cs
+++ Assets/Scripts/DarcyStudio/GameComponent/ForRenderer/AnimatorMaterialBase.cs
@@ -72,9 +72,14 @@ namespace DarcyStudio.GameComponent.ForRenderer
             if (image == null)
             {
                 Debug.LogError ("None image component");
+                return null;
             }
 
-            return image.material;
+            // 因为 image.material 这个材质是共用的 所以直接修改这个材质会导致其他使用该材质的图片也受影响
+            // 这里可以考虑做个池子缓存一下这个手动生成的 material 看具体业务情况吧
+            var newMaterial = Instantiate (image.material);
+            image.material = newMaterial;
+            return newMaterial;
         }
 
         protected Material GetMaterialFromRender ()
@@ -83,8 +88,15 @@ namespace DarcyStudio.GameComponent.ForRenderer
             if (tRenderer == null)
             {
                 Debug.LogError ("None Renderer component");
+                return null;
             }
 
+            // 这里在编辑器非运行模式下会报一个错 只是一个 Unity 的警告： Instantiating material due to calling renderer.material during edit mode. This will leak materials into the scene. You most likely want to use renderer.sharedMaterial instead.
+            // 大致意思是在 edit 模式下 会生成一个材质 所以会造成内存泄露在场景里，如果这时候保存场景了，会将这个材质带入这个场景中，
+            // unity 推荐使用 SharedMaterial，但是我们这个脚本会改变 Material 的属性
+            // 如果使用 SharedMaterial 会改变 Assets 中的 material 这样不小心提交了可能会导致无意识的修改资源中的材质
+            // 鉴于 美术那边使用该脚本一般是做动效，所以还是使用 renderer.material
+            Debug.LogError ("⚠️注意 如果看到该警告，且你在操作场景，请不要保存场景，不要保存场景，不要保存场景！！！，如果只是操作 Prefab️ 可以忽略该警告");
             return tRenderer.material;
         }
 
