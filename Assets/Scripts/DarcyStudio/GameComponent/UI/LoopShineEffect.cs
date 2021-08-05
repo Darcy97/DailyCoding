a0a3a16de69ed2afff2298f9ca0f99df757bf41d
Darcy <Darcy97zh@gmail.com>
2021-08-05 22:04:57 +0800

    [op] Loop shine effect

--- Assets/Scripts/DarcyStudio/GameComponent/UI/LoopShineEffect.cs
+++ Assets/Scripts/DarcyStudio/GameComponent/UI/LoopShineEffect.cs
@@ -17,14 +17,19 @@ namespace DarcyStudio.GameComponent.UI
     {
         [SerializeField] private float loopTime = 1f;
 
-        [Header("编辑器模式是否运行 编辑模式测试时可以勾选上")]
-        [SerializeField] private bool  runInEditorMode = false;
-        private                  float _time;
+#if UNITY_EDITOR
+        [Header ("编辑器模式是否运行 编辑模式测试时可以勾选上")] [SerializeField]
+        private bool runInEditorMode = false;
+#endif
+
+        private float _time;
 
         private void Update ()
         {
+#if UNITY_EDITOR
             if (!Application.isPlaying && !runInEditorMode)
                 return;
+#endif
 
             _time += Time.deltaTime;
             if (_time > loopTime)
